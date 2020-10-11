c6fee6439d6e95126c7e3d313f61912d70d6064b
zezulka000 <zezulka000@yahoo.com>
2020-10-11 13:38:58 +0200

    Init commit

---
+++ src/test/java/pl/zezulka/game/rockpaperscissors/RockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zezulka.game.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
